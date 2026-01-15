9b5a483f2d2124bec621b0d8000a50ee38db8657
Caroline Tenorio <carololiveiratenorio21@gmail.com>
2026-01-15 20:17:57 +0000

    alteração de tamanho no componente categoria

--- frontend/src/components/CardCategoria.jsx
+++ frontend/src/components/CardCategoria.jsx
@@ -1,7 +1,7 @@
 export default function CardCategoria({ tituloCategory, imgCategory }) {
     return (
         <div className="w-[90px] ">
-            <div className="w-[90px] h-[90px] bg-[#d8e2ef] rounded-full flex items-center  justify-center">
+            <div className="w-[70px] h-[70px] bg-[#d8e2ef] rounded-full flex items-center  justify-center">
                 <img className="" src={imgCategory} />
             </div>
             <p className="text-center text-font-cinza flex flex-col mt-[10px]">{tituloCategory}</p>
