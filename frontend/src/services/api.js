e6ac7c2f6c3bae67d16435298ff10b7c61b33b81
Caroline Tenorio <carololiveiratenorio21@gmail.com>
2026-01-21 19:24:11 +0000

    Altereisa

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://loja-fullstack-6cpj.vercel.app'
+    baseURL: 'https://loja-fullstack-6cpj.vercel.app/api'
 })
 
 export default api;
